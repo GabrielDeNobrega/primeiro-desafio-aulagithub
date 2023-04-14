be54db7110654657c4a231db5ca473962d7ceec7
GabrielDeNobrega <sr.gabrielnobrega@hotmail.com>
2023-04-14 13:06:59 -0300

    Criando a funcionalidade de soma.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,29 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+
+		Locale.setDefault(Locale.US);
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("*** Sum Two Integer Numbers ***");
+
+		System.out.print("Insert the first number: ");
+		Integer firstNumber = sc.nextInt();
+		System.out.print("Insert the second number: ");
+		Integer secondNumber = sc.nextInt();
+
+		Integer finalResult = firstNumber + secondNumber;
+
+		System.out.println("The final result of the sum is equal to " + finalResult + ".");
+
+		sc.close();
+
 	}
 
 }
